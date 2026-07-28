dd0df6a978822e56b4a280d303d4a05939e8692d
rosa0922 <rohit.savalagi@netcracker.com>
2026-07-29 01:31:44 +0530

    httpClient to httpResource transition

--- src/app/domains/ticketing/feature-booking/flight-search/flight-search.ts
+++ src/app/domains/ticketing/feature-booking/flight-search/flight-search.ts
@@ -1,9 +1,8 @@
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { form, FormField } from '@angular/forms/signals';
 import { Flight } from '../../data/flight';
-import { initialAircraft } from '../../data/aircraft';
 import { DatePipe, JsonPipe } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
+import { httpResource } from '@angular/common/http';
 
 @Component({
   selector: 'app-flight-search',
@@ -12,39 +11,56 @@ import { HttpClient } from '@angular/common/http';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FlightSearch {
-  private readonly httpClient = inject(HttpClient);
-
   protected readonly filter = signal({
     from: 'Hamburg',
     to: 'Graz',
   });
 
   protected readonly filterForm = form(this.filter);
-  protected readonly flights = signal<Flight[]>([]);
 
   protected readonly selectedFlight = signal<Flight | null>(null);
 
+  protected readonly flightsResource = httpResource<Flight[]>(
+    () => {
+      const filter = {
+        from: this.filter().from,
+        to: this.filter().to,
+      };
+
+      if (!filter.from || !filter.to) return undefined;
+
+      return {
+        url: 'https://demo.angulararchitects.io/api/flight',
+        params: {
+          from: filter.from,
+          to: filter.to,
+        },
+      };
+    },
+    { defaultValue: [] },
+  );
+
+  protected readonly flights = this.flightsResource.value;
+  protected readonly error = this.flightsResource.error;
+  protected readonly isLoading = this.flightsResource.isLoading;
+
   protected search(): void {
-    const url = 'https://demo.angulararchitects.io/api/flight'
-
-    const filter = this.filter();
-
-    const params = {
-      from: filter.from,
-      to: filter.to,
-    }
-
-    this.httpClient.get<Flight[]>(url, { params }).subscribe({
-      next: (flights: Flight[]) => {
-        this.flights.set(flights);
-      },
-      error: (err) => {
-        console.error('Error', err)
-      }
-    });
+    this.flightsResource.reload();
   }
 
   protected select(flight: Flight): void {
     this.selectedFlight.set(flight);
+  }
+
+  protected readonly basket = signal<Record<number, boolean>>({
+    3: true,
+    5: true,
+  });
+
+  protected updateBasket(flightId: number, selected: boolean): void {
+    this.basket.update((basket) => ({
+      ...basket,
+      [flightId]: selected,
+    }));
   }
 }
