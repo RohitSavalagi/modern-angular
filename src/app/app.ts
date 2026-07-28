6546963c5630e481489a68421583ca67c5b3932e
rosa0922 <rohit.savalagi@netcracker.com>
2026-07-28 22:29:42 +0530

    Initial Setup
    - Added Navbar component with toggle functionality for sidebar.
    - Created Sidebar component with navigation links.
    - Updated app component to include Navbar and Sidebar.
    - Modified index.html to include Google Fonts for better typography.
    - Introduced material theme styles for Angular Material components.
    - Enhanced global styles with custom properties for consistent theming.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Navbar } from './shell/navbar/navbar';
+import { Sidebar } from './shell/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [
+    Navbar,
+    Sidebar
+  ]
 })
 export class App {
-  protected readonly title = signal('rxMethod');
+  protected readonly title = signal('Flights');
+
+  protected updateTitle(): void {
+    this.title.set("high end flight");
+    console.log("title updated", this.title());
+  }
 }
