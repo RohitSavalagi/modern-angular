43277cf81ad63adebd4e2ca20a0b1c5a7ac229eb
rosa0922 <rohit.savalagi@netcracker.com>
2026-08-02 00:03:47 +0530

    feat: entities
    --add next flights feature with routing, store, and UI components

---
+++ src/app/domains/ticketing/feature-next-flights/next-flights.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+
+import { NextFlightsOverview } from './next-flights-overview/next-flights-overview';
+
+export const nextFlightRoutes: Routes = [
+  {
+    path: '',
+    component: NextFlightsOverview,
+  },
+];
