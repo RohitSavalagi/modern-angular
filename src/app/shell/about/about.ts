632e81e618ca76f0f19b8207e13dad6e518b21a7
rosa0922 <rohit.savalagi@netcracker.com>
2026-08-02 16:27:53 +0530

    feat: Attribute directive
    --add ClickWithWarning, SimpleTooltip, and Tooltip directives with usage in About component

--- src/app/shell/about/about.ts
+++ src/app/shell/about/about.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { TabbedPane } from "@flight/shared/ui-common/tabbed-pane/tabbed-pane";
 import { Tab } from "@flight/shared/ui-common/tabbed-pane/tab/tab";
+import { ClickWithWarning } from "@flight/shared/ui-common/click-with-warning.directive";
+import { SimpleTooltipDirective } from "@flight/shared/ui-common/simple-tooltip.directive";
+import { TooltipDirective } from "@flight/shared/ui-common/tooltip.directive";
 
 @Component({
   selector: 'app-about',
-  imports: [TabbedPane, Tab],
+  imports: [TabbedPane, Tab, ClickWithWarning, SimpleTooltipDirective, TooltipDirective],
   templateUrl: './about.html',
 })
-export class About {}
+export class About {
+  deleteAll(): void {
+    console.log("Delete all pressed");
+  }
+}
